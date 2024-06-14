3053329c3a3491533eeb873d1d06b3d1e8f1293a
Vojtech Juranek <vojtech.juranek@gmail.com>
2024-06-14 22:45:41 +0200

    Use data from postgres folder

--- qt-sender/src/mnist/main.cpp
+++ qt-sender/src/mnist/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName(RestClient::APP_NAME);
     QSettings settings;
     settings.setValue(RestClient::KEY_REST_URL, "http://localhost:8080/rest"); //URL with Infinispan REST API
-    settings.setValue(MnistReader::KEY_FILE_PATH, "/tmp/mnist/data/t10k-images-idx3-ubyte"); //path to the test MNIST data sample
+    settings.setValue(MnistReader::KEY_FILE_PATH, "postgres/data/t10k-images-idx3-ubyte"); //path to the test MNIST data sample
 
     QGuiApplication app(argc, argv);
     //registers MnistSender class to be available from QML and we can create MnistSender instance there
